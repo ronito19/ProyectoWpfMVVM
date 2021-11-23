56f33e4e3f79867ba620f6034ecf91187668a1ce
ronito19 <ronnie_1019@hotmail.com>
2021-11-23 21:01:13 +0100

    Agregar archivos de proyecto.

---
+++ WpfMVVM-Project/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfMVVM_Project.Commands;
+using WpfMVVM_Project.Models;
+
+namespace WpfMVVM_Project.ViewModels
+{
+    class MainViewModel
+    {
+        
+
+        public TestModel testmodel { set; get; }
+
+
+        public HomeViewModel SelectedViewModel { set; get; }
+
+
+        public ICommand UpdateViewCommand { set; get; }
+
+        public MainViewModel()
+        {
+            testmodel = new TestModel();
+            testmodel.Marca = "Nike";
+            testmodel.Referencia = "1234";
+
+            UpdateViewCommand = new UpdateViewCommand();
+            SelectedViewModel = new HomeViewModel();
+
+        }
+    }
+}
